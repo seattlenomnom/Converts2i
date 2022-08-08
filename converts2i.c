52863a9cc224c9465db2f78f3490448d32238c4f
seattlenomnom <mark.crapser@gmail.com>
2022-08-08 15:28:42 -0700

    initial commit of project files

---
+++ converts2i.c
@@ -0,0 +1,40 @@
+/* converts2i.c
+ * source file for converts2i a program to convert a string representing an
+ * integer to an actual integer
+ *
+ *
+ *
+ * Programmer: Mark Crapser
+ *
+ * Example program from Kochan, Stephen G., "Programming in C 3rd ed.,"
+ * Sams Publishing. Page 229.
+ *
+*/
+
+#include <stdio.h>
+
+
+
+int main(int argc, char *argv[]) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return(0);
+
+}
---
+++ makefile
@@ -0,0 +1,4 @@
+# makefile for converts2i
+#
+converts2i : converts2i.c
+	gcc -g converts2i.c -o ./Build/converts2i
